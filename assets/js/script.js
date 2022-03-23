d3c0214b1964d9aed4dedc30ad5bdd95d3613e57
micah_z <micah.a.zahn@gmail.com>
2022-03-23 10:47:07 -0500

    add drag and drop functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -185,3 +185,66 @@ $("#remove-tasks").on("click", function() {
 
 // load tasks for the first time
 loadTasks();
+
+
+// make lists sortable
+$(".card .list-group").sortable({
+connectWith: $(".card .list-group"),
+scroll: false,
+tolerance: "pointer",
+helper: "clone",
+activate: function(event) {
+  
+},
+deactivate: function(event) {
+  
+},
+over: function(event) {
+  
+},
+out: function(event) {
+  
+},
+update: function(event) {
+var tempArr = [];
+
+  $(this).children().each(function(){
+    var text = $(this)
+    .find("p")
+    .text()
+    .trim();
+
+    var date = $(this)
+    .find("span")
+    .text()
+    .trim();
+
+    tempArr.push({
+      text: text,
+      date: date
+    });
+  });
+
+  var arrName = $(this)
+  .attr("id")
+  .replace("list-","");
+
+  tasks[arrName] = tempArr;
+  saveTasks();
+}
+});
+
+// trash area droppable
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    ui.draggable.remove();
+  },
+  over: function() {
+    
+  },
+  out: function(event, ui) {
+    
+  }
+})
